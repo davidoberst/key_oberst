cd790c9719a669fcd6bcccaa53d73584e05417f5
davidoberst <juandavidbustosperez82@gmail.com>
2025-12-05 12:46:37 -0500

    inicializate project

---
+++ keyOberst.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <windows.h>
+
+int main(){
+ //MAYUS KEYS
+
+ //0x8000 = bit status 1 (on)
+ // if (key) is on then
+ if(GetAsyncKeyState('A') & 0x8000 ){ 
+    
+ }
+
+}
